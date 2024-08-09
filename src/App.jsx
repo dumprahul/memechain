b974807fa5af185dd2ce2de91416b2ec7433a7ac
admin <rahulhacksgood@gmail.com>
2024-08-09 13:09:01 +0530

    hehe

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,54 @@
-// import { BrowserRouter,Routes,Route } from 'react-router-dom';
-// import MemesandTemplates from './pages/MemesandTemplates';
-// import Hero from './pages/Hero';
-// import Wcbox from './components/Wcbox';
-// import ProfileCard from './components/ProfileCard';
-// import PostsSection from './components/ProfileSection';
-// import Cards from './components/Cards';
-// import PostList from './/components/PostList';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import MemesandTemplates from './pages/MemesandTemplates';
+import Hero from './pages/Hero';
+import Wcbox from './components/Wcbox';
+import ProfileCard from './components/ProfileCard';
+import PostsSection from './components/ProfileSection';
+import Cards from './components/Cards';
+import PostList from './/components/PostList';
+import MemeEditor from './tedit/MemeEditor';
 
 
-// const App = () => {
+const App = () => {
 
-//   return (
-//     <div>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route index element={<Hero />} />
-//           <Route path='/home' element={<Hero />} />
-//           <Route path='/memes' element={<MemesandTemplates  />} />
-//           <Route path='/profile' element={
-//               <div className="min-h-screen mt-12 p-6" style={{ backgroundImage: "url('src/assets/anime.jpg')" }} id="wcbox">
-//                   <ProfileCard />
-//                   <PostList />
-//                   <PostList />
-//               </div>
-//              } />
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-
-
-//   )
-
-
-// }
-
-// export default App;
-
-
-
-import React from 'react';
-import MemeEditor from '../src/tedit/MemeEditor';
-
-function App() {
   return (
-    <div className="App">
-      <MemeEditor />
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Hero />} />
+          <Route path='/home' element={<Hero />} />
+          <Route path='/memes' element={<MemesandTemplates  />} />
+          <Route path='/profile' element={
+              <div className="min-h-screen mt-12 p-6" style={{ backgroundImage: "url('src/assets/anime.jpg')" }} id="wcbox">
+                  <ProfileCard />
+                  <PostList />
+                  <PostList />
+              </div>
+             } />
+          <Route path='/memeeditor' element={<MemeEditor  />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+
+
+  )
+
+
 }
 
 export default App;
+
+
+
+// import React from 'react';
+// import MemeEditor from '../src/tedit/MemeEditor';
+
+// function App() {
+//   return (
+//     <div className="App"  style={{ backgroundImage: "url('src/assets/anime.jpg')" }} >
+//       <MemeEditor />
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/tedit/MemeEditor.jsx
+++ src/tedit/MemeEditor.jsx
@@ -10,11 +10,11 @@ const MemeEditor = () => {
   const [stickers, setStickers] = useState([]); // State to manage multiple stickers
 
   const stickerOptions = [
-    'src/assets/stickers/blunt_1_optimized.png', // Replace with actual sticker paths
-    'src/assets/stickers/dogedog_optimized.png',
-    'src/assets/stickers/laser_1_optimized.png',
-    'src/assets/stickers/sunglass_optimized.png',
-    'src/assets/stickers/vibecat_op.jpg',
+    '/stickers/sticker1.png', // Replace with actual sticker paths
+    '/stickers/sticker2.png',
+    '/stickers/sticker3.png',
+    '/stickers/sticker4.png',
+    '/stickers/sticker5.png',
   ];
 
   const handleImageUpload = (e) => {
@@ -28,32 +28,27 @@ const MemeEditor = () => {
 
   const handleDownload = () => {
     const imageElement = document.getElementById('uploadedImage');
-    
+  
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
-  
-    // Set canvas dimensions to match the actual dimensions of the image
+
     const actualWidth = imageElement.naturalWidth;
     const actualHeight = imageElement.naturalHeight;
     canvas.width = actualWidth;
     canvas.height = actualHeight;
-  
-    // Draw the image on the canvas at its natural size
+
     ctx.drawImage(imageElement, 0, 0, actualWidth, actualHeight);
-  
-    // Scale factor to adjust positions of text and stickers according to the actual image size
+
     const scaleX = actualWidth / imageElement.width;
     const scaleY = actualHeight / imageElement.height;
-  
-    // Render all text boxes
+
     textBoxes.forEach((box) => {
       ctx.font = `${box.fontSize * scaleX}px ${box.font}`;
       ctx.fillStyle = box.color;
       ctx.textAlign = 'center';
       ctx.fillText(box.text, box.x * scaleX, (box.y + parseInt(box.fontSize)) * scaleY);
     });
-  
-    // Render all stickers
+
     stickers.forEach(sticker => {
       const stickerImage = new Image();
       stickerImage.src = sticker.src;
@@ -61,13 +56,13 @@ const MemeEditor = () => {
         ctx.drawImage(stickerImage, sticker.x * scaleX, sticker.y * scaleY, stickerImage.width * scaleX, stickerImage.height * scaleY);
       };
     });
-  
+
     setTimeout(() => {
       const link = document.createElement('a');
       link.download = 'meme.png';
       link.href = canvas.toDataURL();
       link.click();
-    }, 1000); // Small delay to ensure all elements are drawn
+    }, 1000);
   };
 
   const handleDeleteText = (index) => {
@@ -102,170 +97,167 @@ const MemeEditor = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-8">
-      <div className="flex border-2 border-gray-300 p-8 rounded-lg w-[900px] h-[600px]">
-        <div className="w-2/3 flex items-center justify-center">
-          {image ? (
-            <div className="relative">
-              <img
-                id="uploadedImage"
-                src={image}
-                alt="Meme"
-                className="max-w-full max-h-full border-4 border-gray-500 rounded-md"
-              />
-              {textBoxes.map((box, index) => (
-                <Rnd
-                  key={index}
-                  bounds="parent"
-                  position={{ x: box.x, y: box.y }}
-                  onDragStop={(e, d) =>
-                    updateTextBox(index, { ...box, x: d.x, y: d.y })
-                  }
-                  className="absolute"
-                  style={{
-                    fontFamily: box.font,
-                    fontSize: `${box.fontSize}px`,
-                    color: box.color,
-                    border: '2px dashed rgba(255, 255, 255, 0.5)', // Transparent border for dragging
-                    padding: '8px',
-                  }}
-                >
-                  <div>
-                    <input
-                      type="text"
-                      value={box.text}
-                      onChange={(e) =>
-                        updateTextBox(index, { ...box, text: e.target.value })
-                      }
-                      style={{
-                        fontFamily: box.font,
-                        fontSize: `${box.fontSize}px`,
-                        color: box.color,
-                        backgroundColor: 'transparent',
-                        border: 'none',
-                        outline: 'none',
-                      }}
-                    />
-                    <button
-                      onClick={() => handleDeleteText(index)}
-                      className="absolute top-0 right-0 bg-red-500 text-white rounded-full px-2 py-1 text-xs"
-                    >
-                      X
-                    </button>
-                  </div>
-                </Rnd>
-              ))}
-              {stickers.map((sticker, index) => (
-                <Rnd
-                  key={index}
-                  bounds="parent"
-                  position={{ x: sticker.x, y: sticker.y }}
-                  onDragStop={(e, d) =>
-                    setStickers(stickers.map((s, i) =>
-                      i === index ? { ...s, x: d.x, y: d.y } : s
-                    ))
-                  }
-                  className="absolute"
-                  style={{
-                    border: '2px dashed rgba(255, 255, 255, 0.5)', // Transparent border for dragging
-                    padding: '8px',
-                  }}
-                >
-                  <img src={sticker.src} alt="Sticker" className="max-w-full max-h-full" />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-8" style={{ backgroundImage: "url('src/assets/anime.jpg')" }}>
+      {/* Buttons Container */}
+      <h1 className="text-3xl font-bold mb-6 font-family" id="fontf" >Meme Editor</h1>
+      <div className="flex justify-center items-center space-x-4 mb-4" >
+        <label htmlFor="file-upload" className="btn btn-outline btn-secondary">
+          Choose File
+        </label>
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={handleImageUpload}
+          className="hidden"
+        />
+
+        <button onClick={addTextBox} className="btn btn-outline btn-secondary">
+          Add Text
+        </button>
+
+        <select
+          value={font}
+          onChange={(e) => setFont(e.target.value)}
+          className="btn btn-outline btn-secondary"
+        >
+          <option value="Arial">Arial</option>
+          <option value="Verdana">Verdana</option>
+          <option value="Times New Roman">Times New Roman</option>
+          <option value="Courier New">Courier New</option>
+          <option value="Georgia">Georgia</option>
+        </select>
+
+        <select
+          value={fontSize}
+          onChange={(e) => setFontSize(e.target.value)}
+          className="btn btn-outline btn-secondary"
+        >
+          <option value="20">20</option>
+          <option value="30">30</option>
+          <option value="40">40</option>
+          <option value="50">50</option>
+          <option value="60">60</option>
+        </select>
+
+        <select
+          value={textColor}
+          onChange={(e) => setTextColor(e.target.value)}
+          className="btn btn-outline btn-secondary"
+        >
+          <option value="#FFFFFF">White</option>
+          <option value="#000000">Black</option>
+          <option value="#FF0000">Red</option>
+          <option value="#00FF00">Green</option>
+          <option value="#0000FF">Blue</option>
+        </select>
+
+        <select
+          onChange={(e) => addSticker(e.target.value)}
+          className="btn btn-outline btn-secondary"
+        >
+          <option value="">Select a sticker</option>
+          {stickerOptions.map((stickerPath, index) => (
+            <option key={index} value={stickerPath}>
+              Sticker {index + 1}
+            </option>
+          ))}
+        </select>
+
+        {image && (
+          <button
+            onClick={handleDownload}
+            className="btn btn-outline btn-secondary"
+          >
+            Download Meme
+          </button>
+        )}
+      </div>
+
+      {/* Meme Layout Container */}
+      <div className="flex border-2 border-gray-300 p-8 rounded-lg w-[900px] h-[600px] items-center justify-center"style={{ backgroundImage: "url('src/assets/white-bg.jpg')" }} >
+        {image ? (
+          <div className="relative">
+            <img
+              id="uploadedImage"
+              src={image}
+              alt="Meme"
+              className="max-w-full max-h-full border-4 border-gray-500 rounded-md"
+            />
+            {textBoxes.map((box, index) => (
+              <Rnd
+                key={index}
+                bounds="parent"
+                position={{ x: box.x, y: box.y }}
+                onDragStop={(e, d) =>
+                  updateTextBox(index, { ...box, x: d.x, y: d.y })
+                }
+                className="absolute"
+                style={{
+                  fontFamily: box.font,
+                  fontSize: `${box.fontSize}px`,
+                  color: box.color,
+                  border: '2px dashed rgba(255, 255, 255, 0.5)', // Transparent border for dragging
+                  padding: '8px',
+                }}
+              >
+                <div>
+                  <input
+                    type="text"
+                    value={box.text}
+                    onChange={(e) =>
+                      updateTextBox(index, { ...box, text: e.target.value })
+                    }
+                    style={{
+                      fontFamily: box.font,
+                      fontSize: `${box.fontSize}px`,
+                      color: box.color,
+                      backgroundColor: 'transparent',
+                      border: 'none',
+                      outline: 'none',
+                    }}
+                  />
                   <button
-                    onClick={() => handleDeleteSticker(index)}
-                    className="absolute top-0 right-0 bg-red-500 text-white rounded-full px-2 py-1 text-xs"
+                    onClick={() => handleDeleteText(index)}
+                    className="btn btn-outline btn-secondary"
                   >
                     X
                   </button>
-                </Rnd>
-              ))}
-            </div>
-          ) : (
-            <div className="text-gray-400">No image uploaded yet</div>
-          )}
-        </div>
-
-        <div className="w-1/3 flex flex-col items-start justify-start ml-8 space-y-4">
-          <input type="file" accept="image/*" onChange={handleImageUpload} className="mb-4" />
-
-          <button
-            onClick={addTextBox}
-            className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600"
-          >
-            Add Text
-          </button>
-
-          <div className="flex flex-col space-y-4">
-            {/* Font Dropdown */}
-            <select
-              value={font}
-              onChange={(e) => setFont(e.target.value)}
-              className="p-2 border border-gray-400 rounded w-full"
-            >
-              <option value="Arial">Arial</option>
-              <option value="Verdana">Verdana</option>
-              <option value="Times New Roman">Times New Roman</option>
-              <option value="Courier New">Courier New</option>
-              <option value="Georgia">Georgia</option>
-            </select>
-
-            {/* Font Size Dropdown */}
-            <select
-              value={fontSize}
-              onChange={(e) => setFontSize(e.target.value)}
-              className="p-2 border border-gray-400 rounded w-full"
-            >
-             <option value="5">5</option>
-              <option value="10">10</option>
-              <option value="20">20</option>
-              <option value="30">30</option>
-              <option value="40">40</option>
-              <option value="50">50</option>
-            </select>
-
-            {/* Color Dropdown */}
-            <select
-              value={textColor}
-              onChange={(e) => setTextColor(e.target.value)}
-              className="p-2 border border-gray-400 rounded w-full"
-            >
-              <option value="#FFFFFF">White</option>
-              <option value="#000000">Black</option>
-              <option value="#FF0000">Red</option>
-              <option value="#00FF00">Green</option>
-              <option value="#0000FF">Blue</option>
-            </select>
-
-            {/* Sticker Dropdown */}
-            <select
-              onChange={(e) => addSticker(e.target.value)}
-              className="p-2 border border-gray-400 rounded w-full"
-            >
-              <option value="">Select a sticker</option>
-              {stickerOptions.map((stickerPath, index) => (
-                <option key={index} value={stickerPath}>
-                  Sticker {index + 1}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div className="flex space-x-4">
-            {/* Download Button */}
-            {image && (
-              <button
-                onClick={handleDownload}
-                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                </div>
+              </Rnd>
+            ))}
+            {stickers.map((sticker, index) => (
+              <Rnd
+                key={index}
+                bounds="parent"
+                position={{ x: sticker.x, y: sticker.y }}
+                onDragStop={(e, d) =>
+                  setStickers(stickers.map((s, i) =>
+                    i === index ? { ...s, x: d.x, y: d.y } : s
+                  ))
+                }
+                className="absolute"
+                style={{
+                  border: '2px dashed rgba(255, 255, 255, 0.5)', // Transparent border for dragging
+                  padding: '8px',
+                }}
               >
-                Download Meme
-              </button>
-            )}
+                <img src={sticker.src} alt="Sticker" className="max-w-full max-h-full" />
+                <button
+                  onClick={() => handleDeleteSticker(index)}
+                  className="btn btn-outline btn-secondary"
+                >
+                  X
+                </button>
+              </Rnd>
+            ))}
           </div>
-        </div>
+        ) : (
+          <div className="text-gray-400">No image uploaded yet</div>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default MemeEditor;
