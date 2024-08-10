a8259252be0d1db88ed01ec21f3306b7b76c1c89
Kaushikh76 <63700909+Kaushikh76@users.noreply.github.com>
2024-08-10 21:38:01 +0000

    512

--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -37,6 +37,18 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
+         <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+           <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
+        />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
