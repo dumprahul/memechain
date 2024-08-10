e5b17423891ed7a61f3024681e017e698b67b70f
Kaushikh76 <63700909+Kaushikh76@users.noreply.github.com>
2024-08-10 18:39:23 +0000

    add logo

--- fe/src/app/Navbar.jsx
+++ fe/src/app/Navbar.jsx
@@ -6,35 +6,31 @@ import {
   useSignerStatus,
   useUser,
 } from "@account-kit/react";
+import Image from "next/image";
 
 const Navbar = () => {
   const { openAuthModal } = useAuthModal();
-  const {logOut} =useLogout();
   const user = useUser();
   return (
     <div className="navbar bg-[#f5f589] fixed top-0 left-0 w-full z-10 py-4 flex justify-between items-center">
       {" "}
       {/* Use flexbox and justify-between */}
-      <div className="navbar-start">
-        <a className="btn btn-ghost text-xl text-black" id="font">
+      <div className="flex space-x-2 my-auto ml-6">
+        <Image src="/logo-one.png" alt="logo" width={50} height={50} />{" "}
+        {/* Added logo */}
+        <p className="text-xl text-black my-auto" id="font">
           MEMECAST
-        </a>
+        </p>
       </div>
       <div className="navbar-end pr-4">
         {" "}
         {/* Added padding-right to navbar-end */}
-        {user != null ? (
-          <button className="btn text-black bg-white hover:bg-gray-300 cursor-default" onClick={logOut}>
-            {user.address.slice(0, 6)}...{user.address.slice(-4)}
-          </button>
-        ) : (
-          <button
-            className="btn text-black bg-white hover:bg-gray-300"
-            onClick={openAuthModal}
-          >
-            Connect Wallet
-          </button>
-        )}
+        <button
+          className="btn text-black bg-white hover:bg-gray-300"
+          onClick={openAuthModal}
+        >
+          Connect Wallet
+        </button>
       </div>
     </div>
   );
