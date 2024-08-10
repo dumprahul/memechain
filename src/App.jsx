249c9478fad1ddb7e1f82b85efeb813b208ee646
admin <rahulhacksgood@gmail.com>
2024-08-10 10:32:40 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// src/App.jsx
+
 
 
 import React from 'react';
@@ -22,16 +22,6 @@ function App() {
 
 export default App;
 
-// import React from 'react'
-// import MemeEditor from './tedit/MemeEditor'
-
-// const App = () => {
-//   return (
-//     <MemeEditor />
-//   )
-// }
-
-// export default App;
 
 
 
@@ -56,59 +46,3 @@ export default App;
 
 
 
-
-
-// import { BrowserRouter,Routes,Route } from 'react-router-dom';
-// import MemesandTemplates from './pages/MemesandTemplates';
-// import Hero from './pages/Hero';
-// import Wcbox from './components/Wcbox';
-// import ProfileCard from './components/ProfileCard';
-// import PostsSection from './components/ProfileSection';
-// import Cards from './components/Cards';
-// import PostList from './/components/PostList';
-// import MemeEditor from './tedit/MemeEditor';
-
-
-// const App = () => {
-
-//   return (
-//     <div>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route index element={<Hero />} />
-//           <Route path='/home' element={<Hero />} />
-//           <Route path='/memes' element={<MemesandTemplates  />} />
-//           <Route path='/profile' element={
-//               <div className="min-h-screen mt-12 p-6" style={{ backgroundImage: "url('src/assets/anime.jpg')" }} id="wcbox">
-//                   <ProfileCard />
-//                   <PostList />
-//                   <PostList />
-//               </div>
-//              } />
-//           <Route path='/memeeditor' element={<MemeEditor  />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-
-
-//   )
-
-
-// }
-
-// export default App;
-
-
-
-// import React from 'react';
-// import MemeEditor from '../src/tedit/MemeEditor';
-
-// function App() {
-//   return (
-//     <div className="App"  style={{ backgroundImage: "url('src/assets/anime.jpg')" }} >
-//       <MemeEditor />
-//     </div>
-//   );
-// }
-
-// export default App;
