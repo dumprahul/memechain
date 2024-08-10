fec7792d3396f4245d164ec09da7c6fb9fb62f2e
Kaushikh76 <63700909+Kaushikh76@users.noreply.github.com>
2024-08-10 20:05:10 +0000

    add aura chain

--- fe/src/utils/config.ts
+++ fe/src/utils/config.ts
@@ -1,7 +1,26 @@
 import { AlchemyAccountsUIConfig, createConfig } from "@account-kit/react";
-import { arbitrumSepolia, baseSepolia } from "@account-kit/infra";
+import { baseSepolia, defineAlchemyChain } from "@account-kit/infra";
+import { defineChain } from "viem";
 import { QueryClient } from "@tanstack/react-query";
-
+const auraChain = defineChain({
+  id: 88759,
+  name: "Aura Chain",
+  nativeCurrency: { name: "ETH", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: { http: ["https://rpc-aurachain-kooclv2ptj.t.conduit.xyz"] },
+  },
+  blockExplorers: {
+    default: {
+      name: "Blockscout Explorer",
+      url: "https://explorer-aurachain-kooclv2ptj.t.conduit.xyz/",
+    },
+  },
+  contracts: {},
+});
+const alchemyAuraChain = defineAlchemyChain({
+  chain: auraChain,
+  rpcBaseUrl: "",
+});
 const uiConfig: AlchemyAccountsUIConfig = {
   illustrationStyle: "outline",
   auth: {
@@ -16,13 +35,8 @@ const uiConfig: AlchemyAccountsUIConfig = {
 export const config = createConfig(
   {
     apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!,
-    chain: baseSepolia,
-    chains: [
-      { chain: baseSepolia },
-      {
-        chain: arbitrumSepolia,
-      },
-    ],
+    chain: alchemyAuraChain,
+    chains: [{ chain: alchemyAuraChain }, { chain: baseSepolia }],
     ssr: true,
   },
   uiConfig
