e2d618df3e2af7d568ad1f0fcbf1ed64dae26401
Kaushikh76 <63700909+Kaushikh76@users.noreply.github.com>
2024-08-11 09:10:35 +0000

    add scriprts

--- fe/src/utils/supabase/read/getAllMemes.ts
+++ fe/src/utils/supabase/read/getAllMemes.ts
@@ -3,30 +3,20 @@ const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_API_KEY ?? "";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export default async function getMeme(
-  memeId: string
+export default async function getAllMemes(
+  created_by: string
 ): Promise<{ success: boolean; data: any }> {
   try {
-    let { data: meme, error } = await supabase
+    let { data: memes, error } = await supabase
       .from("memes")
-      .select(
-        `*,
-      template (
-        id
-      ),
-      category (
-        id,
-        label
-       ),`
-      )
-      .eq("id", memeId);
-    console.log(meme);
-    if (meme == null || meme?.length == 0)
+      .select(`id,image`)
+    console.log(memes);
+    if (memes == null || memes?.length == 0)
       return { success: false, data: "Meme not found" };
     else
       return {
         success: true,
-        data: meme[0],
+        data: memes
       };
   } catch (e) {
     console.log(e);
--- fe/src/utils/supabase/read/getMemesByCreator.ts
+++ fe/src/utils/supabase/read/getMemesByCreator.ts
@@ -4,29 +4,20 @@ const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_API_KEY ?? "";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default async function getMeme(
-  memeId: string
+  created_by: string
 ): Promise<{ success: boolean; data: any }> {
   try {
-    let { data: meme, error } = await supabase
+    let { data: memes, error } = await supabase
       .from("memes")
-      .select(
-        `*,
-      template (
-        id
-      ),
-      category (
-        id,
-        label
-       ),`
-      )
-      .eq("id", memeId);
-    console.log(meme);
-    if (meme == null || meme?.length == 0)
+      .select(`id,image`)
+      .eq("created_by", created_by);
+    console.log(memes);
+    if (memes == null || memes?.length == 0)
       return { success: false, data: "Meme not found" };
     else
       return {
         success: true,
-        data: meme[0],
+        data: memes
       };
   } catch (e) {
     console.log(e);
